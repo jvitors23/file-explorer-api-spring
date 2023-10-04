835d052f2e7968dab1a1da72c473b93ed5a1a618
José Vitor <jvss23031999@gmail.com>
2023-10-04 18:48:01 +0100

    Adding spring initializr project

---
+++ src/test/java/com/backend/fileexplorer/FileexplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.fileexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileexplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
